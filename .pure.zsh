66e8ad9585cd8f4a8bff5b30aaf6a3c9678fb822
Kristian Andersen <me@kristianandersen.me>
2013-10-23 07:45:39 +0200

    Updated with newest version of pure

--- .pure.zsh
+++ .pure.zsh
@@ -3,7 +3,6 @@
 # https://github.com/sindresorhus/pure
 # MIT License
 
-
 # For my own and others sanity
 # git:
 # %b => current branch
@@ -17,54 +16,84 @@
 # %m => shortname host
 # %(?..) => prompt conditional - %(condition.true.false)
 
+# fastest possible way to check if repo is dirty
+prompt_pure_git_dirty() {
+	# check if we're in a git repo
+	command git rev-parse --is-inside-work-tree &>/dev/null || return
+	# check if it's dirty
+	command git diff --quiet --ignore-submodules HEAD &>/dev/null
 
-() {
+	(($? == 1)) && echo '*'
+}
+
+# displays the exec time of the last command if set threshold was exceeded
+prompt_pure_cmd_exec_time() {
+	local stop=$(date +%s)
+	local start=${cmd_timestamp:-$stop}
+	integer elapsed=$stop-$start
+	(($elapsed > ${PURE_CMD_MAX_EXEC_TIME:=5})) && echo ${elapsed}s
+}
+
+prompt_pure_preexec() {
+	cmd_timestamp=$(date +%s)
+
+	# shows the current dir and executed command in the title when a process is active
+	print -Pn "\e]0;"
+	echo -nE "$PWD:t: $2"
+	print -Pn "\a"
+}
+
+# string length ignoring ansi escapes
+prompt_pure_string_length() {
+	echo ${#${(S%%)1//(\%([KF1]|)\{*\}|\%[Bbkf])}}
+}
+
+prompt_pure_precmd() {
+	# shows the full path in the title
+	print -Pn '\e]0;%~\a'
+
+	# git info
+	vcs_info
+
+	local prompt_pure_preprompt='\n%F{blue}%~%F{242}$vcs_info_msg_0_`prompt_pure_git_dirty` $prompt_pure_username%f %F{yellow}`prompt_pure_cmd_exec_time`%f'
+	print -P $prompt_pure_preprompt
+
+	# check async if there is anything to pull
+	{
+		# check if we're in a git repo
+		command git rev-parse --is-inside-work-tree &>/dev/null &&
+		# check check if there is anything to pull
+		command git fetch &>/dev/null &&
+		# check if there is an upstream configured for this branch
+		command git rev-parse --abbrev-ref @'{u}' &>/dev/null &&
+		(( $(command git rev-list --right-only --count HEAD...@'{u}' 2>/dev/null) > 0 )) &&
+		# some crazy ansi magic to inject the symbol into the previous line
+		print -Pn "\e7\e[A\e[1G\e[`prompt_pure_string_length $prompt_pure_preprompt`C%F{cyan}⇣%f\e8"
+	} &!
+
+	# reset value since `preexec` isn't always triggered
+	unset cmd_timestamp
+}
+
+
+prompt_pure_setup() {
+	prompt_opts=(cr subst percent)
+
 	autoload -Uz add-zsh-hook
 	autoload -Uz vcs_info
 
-	add-zsh-hook precmd pure_precmd
-	add-zsh-hook preexec pure_preexec
+	add-zsh-hook precmd prompt_pure_precmd
+	add-zsh-hook preexec prompt_pure_preexec
 
-	zstyle ':vcs_info:*' enable git # You can add hg too if needed: `git hg`
+	zstyle ':vcs_info:*' enable git
 	zstyle ':vcs_info:git*' formats ' %b'
 	zstyle ':vcs_info:git*' actionformats ' %b|%a'
 
-	# enable prompt substitution
-	setopt PROMPT_SUBST
-
-	# only show username if not default
-	[ $USER != "$PURE_DEFAULT_USERNAME" ] && local username='%n@%m '
-
-	# fastest possible way to check if repo is dirty
-	pure_git_dirty() {
-		# check if we're in a git repo
-		command git rev-parse --is-inside-work-tree &>/dev/null || return
-		# check if it's dirty
-		command git diff --quiet --ignore-submodules HEAD &>/dev/null; [ $? -eq 1 ] && echo '*'
-	}
-
-	# displays the exec time of the last command if set threshold was exceeded
-	pure_cmd_exec_time() {
-		local stop=`date +%s`
-		local start=${cmd_timestamp:-$stop}
-		let local elapsed=$stop-$start
-		[ $elapsed -gt "${PURE_CMD_MAX_EXEC_TIME:=5}" ] && echo ${elapsed}s
-	}
-
-	pure_preexec() {
-		cmd_timestamp=`date +%s`
-	}
-
-	pure_precmd() {
-		vcs_info
-		# add `%*` to display the time
-		print -P '\n%F{blue}%~%F{8}$vcs_info_msg_0_`pure_git_dirty` $username%f %F{yellow}`pure_cmd_exec_time`%f'
-		# reset value since `preexec` isn't always triggered
-		unset cmd_timestamp
-	}
+	# show username@host if logged in through SSH
+	[[ "$SSH_CONNECTION" != '' ]] && prompt_pure_username='%n@%m '
 
 	# prompt turns red if the previous command didn't exit with 0
 	PROMPT='%(?.%F{magenta}.%F{red})❯%f '
-	# can be disabled:
-	# PROMPT='%F{magenta}❯%f '
 }
+
+prompt_pure_setup "$@"
