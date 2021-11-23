5b6eb7e9862ec6f18a6662ee60698c3315ecd179
Kristian Andersen <hello@kristian.co>
2021-11-23 21:37:40 +0100

    Fixed git completion

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for s in .vimrc .vim .git-prompt.sh .bash-ps1.sh .bash_profile .bash_completion .bashrc .inputrc .gitconfig .gitexcludes .hyper.js git-completion.bash
+for s in .vimrc .vim .git-prompt.sh .bash-ps1.sh .bash_profile .bash_completion .bashrc .inputrc .gitconfig .gitexcludes .hyper.js .git-completion.bash
 do
 	original="/Users/ksmandersen/Code/ksmandersen/dotfiles/$s"
 	target="/Users/ksmandersen/$s"
